2d1ebf213a95ef2d91e00a5f73690e618a0db0b1
OmarHafez1 <omaromarahmedhappy@gmail.com>
2024-12-25 09:47:33 +0200

    Wrote the frontend for showing the books for the specific user in the homepage.

--- index.js
+++ index.js
@@ -32,12 +32,16 @@ db.connect((err) => {
 // Constants
 const BASE_URL = "https://openlibrary.org";
 const LOCAL_URL = process.env.LOCAL_URL || `http://localhost:${port}`;
+var user_id = 1;
 
 // Routes
 
 // Home Route
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to the Book Notes App</h1>");
+app.get("/", async (req, res) => {
+  const books = await (await fetch(`${LOCAL_URL}/api/user_books?user_id=${user_id}`)).json();  
+  res.render('index.ejs', {
+    books: books,
+  });
 });
 
 // Add User
@@ -156,15 +160,16 @@ app.get("/api/user_books", async (req, res) => {
 
   try {
     const result = await db.query(
-      `SELECT ub.book_key, ub.rating, ub.note, ub.read_date, 
-              b.title, b.cover_id 
+      `SELECT ub.book_key, ub.rating, ub.note, TO_CHAR(ub.read_date, 'DD/MM/YYYY') AS read_date, 
+              b.title, b.cover_id, 
+              ARRAY(SELECT a.name FROM authors a WHERE a.book_key = b.key) AS authors 
        FROM user_book ub 
        JOIN books b ON ub.book_key = b.key 
        WHERE ub.user_id = $1`,
       [user_id]
     );
-
     res.status(200).json(result.rows);
+    
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Failed to fetch user books" });
